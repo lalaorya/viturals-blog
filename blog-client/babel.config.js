d7785cb05a8d3d4600f6968098719c5d3c4c3d2b
virtual <sangmu.hhj@gmail.com>
2021-03-19 12:20:08 +0800

    第一次提交博客管理系统vue源码

--- blog-client/babel.config.js
+++ blog-client/babel.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    '@vue/app'
+  ],
+  plugins: [
+    ["prismjs", {
+      "languages": ["javascript", "java","css", "markup"],
+      "plugins": ["line-numbers"], //配置显示行号插件
+      "theme": "tomorrow", //主体名称
+      "css": true
+    }]
   ]
 }
--- blog-client/src/App.vue
+++ blog-client/src/App.vue
@@ -1,32 +1,34 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <router-view v-if="isRouterAlive"></router-view>
     </div>
-    <router-view/>
-  </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+    name: 'App',
+    provide() {
+        //父组件中通过provide来提供变量，在子组件中通过inject来注入变量。
+        return {
+            reload: this.reload
+        };
+    },
+    data() {
+        return {
+            isRouterAlive: true //控制视图是否显示的变量
+        };
+    },
+    methods: {
+        reload() {
+            this.isRouterAlive = false; //先关闭，
+            this.$nextTick(function() {
+                this.isRouterAlive = true; //再打开
+            });
+        }
     }
-  }
-}
+};
+</script>
+<style>
+@import './assets/css/main.css';
+@import './assets/css/color-dark.css'; /*深色主题*/
+/*@import "./assets/css/theme-green/color-green.css";   浅绿色主题*/
 </style>

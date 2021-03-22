676badf3bd65621ff980bb2ae45feadd6dc87add
virtual <sangmu.hhj@gmail.com>
2021-03-22 23:55:56 +0800

    实现了登录和注销功能，并于后端实现了联调测试

--- blog-manage/src/main.js
+++ blog-manage/src/main.js
@@ -2,8 +2,8 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css'; // 默认主题
-// import './assets/css/theme-green/index.css'; // 浅绿色主题
+// import 'element-ui/lib/theme-chalk/index.css'; // 默认主题
+import './assets/css/theme-green/index.css'; // 浅绿色主题
 import './assets/css/icon.css';
 import './components/common/directives';
 import 'babel-polyfill';
